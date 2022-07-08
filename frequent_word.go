7280bbfb1a9f0a75bd3548f5cd12e3cb2df938a1
AdityaGadekar <aadityagadekar@gmail.com>
2022-07-08 17:30:18 +0530

    Second commmitt

--- frequent_word.go
+++ frequent_word.go
@@ -4,25 +4,28 @@ import (
 	"fmt"
 	"strings"
 )
-func frequent(str string) map[string]int {
+func frequent(str string) (string , int) {
 	sentence:=strings.Fields(str)
 	m:=make(map[string]int)
 
 	for _, word := range sentence{
 		m[word]=m[word]+1
 	}
-
-	return m	
+	max:=0
+    word:=""
+	for key, value := range m {
+     if value>max{
+		max=value
+		word=key
+	 }
+	}
+return word, max
 
 }
 func main(){
 	sent1 := "qwe asd qwe asd zxc yui qwe"
-	for key , value := range frequent(sent1){
-		fmt.Println(key,"--",value)
-		max:=0
-		if value>max{
-			max = value
-			fmt.Println(key,"word has highest frequency of",max)
-		}
-}
+	// for key , value := range frequent(sent1){
+	// 	fmt.Println(key,"--",value)
+	key, value:=frequent(sent1)
+	fmt.Println(key,"word has highest frequency of",value)
 }
